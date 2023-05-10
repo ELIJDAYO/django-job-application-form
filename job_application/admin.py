bc64d484aebb04aaab29191adc19cb66bc006fe3
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-10 11:18:42 +0800

    Revert "Created admin and access admin interface"
    This reverts commit 505b7e31

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from .models import Form
 
+admin.site.register(Form)
 
-class FormAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "email")
-    search_fields = ("first_name", "last_name", "email")
-    list_filter = ("date", "occupation")
-    # - for reverse
-    ordering = ("-first_name", )
-    readonly_fields = ("occupation",)
-
-
-# updates the layout of form page
-admin.site.register(Form, FormAdmin)
