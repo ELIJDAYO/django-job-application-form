6ee97bdb4ab53614e7c1ce8be03a07f125cebb43
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-10 10:15:43 +0800

    Can send email after form submission

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+# similar to smtp in flask
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -19,6 +21,10 @@ def index(request):
                                 email=email,
                                 date=date,
                                 occupation=occupation)
+
+            message_body = f"A new job application was submitted. Thank you {first_name} {last_name}"
+            email_message = EmailMessage("Form submission confirmation", message_body, to=[email])
+            email_message.send()
 
             messages.success(request, "Form Submitted Successfully")
 
